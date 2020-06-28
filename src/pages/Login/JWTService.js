7218c7370b216d84522cdd974bf875ba393e119b
R. A. Souza <alves.wm@gmail.com>
2020-06-28 15:43:25 -0300

    implements JWT service

--- src/pages/Login/JWTService.js
+++ src/pages/Login/JWTService.js
@@ -1,5 +1,16 @@
 import jwt from "jsonwebtoken";
+import storage from 'localforage';
 
-export const parseJWT = (hash) => {
-  return jwt.decode(hash.substr(1));
-};
+
+const saveJWT = async (hash) => {
+  const token = hash.substr(1);
+  const decoded = jwt.decode(token);
+
+  try {
+    await storage.setItem('jwt', token)
+    await storage.setItem('login', decoded)
+  } 
+  catch (err) {
+    console.log(err);
+  }
+}
