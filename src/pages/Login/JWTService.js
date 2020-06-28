a3f2a99d53044e48d62b5669f6db5012de4eacfa
R. A. Souza <alves.wm@gmail.com>
2020-06-28 17:41:36 -0300

    lints

--- src/pages/Login/JWTService.js
+++ src/pages/Login/JWTService.js
@@ -1,12 +1,6 @@
 import storage from 'local-storage';
 
 export const saveJWT = (hash) => {
-  const token = hash.substr(1);
-
-  try {
-    storage.set('jwt', token)
-  } 
-  catch (err) {
-    console.log(err);
-  }
+  const token = hash.substr(1); // Removes the initial # fragment
+  storage.set('jwt', token)
 }
