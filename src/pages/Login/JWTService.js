1f0ca586382b86d2bb2a99637f00c669d22616ed
Rodrigo Souza <alves.wm@gmail.com>
2020-07-07 00:35:31 -0300

    implements JWT parsing upon login only. Fixes #7 (#13)
    * change jwt lib
    
    * defines new format of env.js
    
    * implements JWT parsing upon login only

--- src/pages/Login/JWTService.js
+++ src/pages/Login/JWTService.js
@@ -1,6 +1,27 @@
-import storage from 'local-storage';
+import jwt from "jsonwebtoken";
+import jwksClient from "jwks-rsa";
+import storage from "local-storage";
 
 export const saveJWT = (hash) => {
   const token = hash.substr(1); // Removes the initial # fragment
-  storage.set('jwt', token)
-}
+  storage.set("jwt", token);
+
+  const client = jwksClient({
+    strictSsl: false, // Default value
+    jwksUri: window.JWKS_URI,
+  });
+
+  client.getSigningKey(window.JWKS_KID, (err, key) => {
+    if (err) {
+      console.error(err);
+      window.location = `${window.USERS_URL}/auth`;
+      return;
+    }
+
+    const signingKey = key.getPublicKey();
+    const decoded = jwt.verify(token, signingKey);
+
+    storage.set("login", decoded);
+    window.location = "/";
+  });
+};
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,19 +1,3 @@
-import jwt from "jsonwebtoken";
 import storage from "local-storage";
-import jwkToPem from "jwk-to-pem";
 
-export const isAuthenticated = () => {  
-  try {
-    const pem = jwkToPem(window.JWKS);
-    const token = storage.get('jwt');
-
-    const decoded = jwt.verify(token, pem);
-    storage.set("login", decoded);
-
-    return true;
-  } catch (err) {
-    console.log(err);
-
-    return false;
-  }
-}
+export const isAuthenticated = () => storage.get('login') != null
