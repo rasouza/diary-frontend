829a71f7803c2a3e9d2388d534724a81b386ae49
R. A. Souza <alves.wm@gmail.com>
2020-07-07 00:10:33 -0300

    implements JWT parsing upon login only

--- src/pages/Login/Callback.js
+++ src/pages/Login/Callback.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
 import { saveJWT } from './JWTService';
 
 const Callback = props => {
   saveJWT(props.location.hash);
   
-  return <Redirect to="/" />
+  return <div />
 }
 
 export default Callback
