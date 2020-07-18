348ff532df89b0dd49c303e5b0df79545357d6ba
R. A. Souza <alves.wm@gmail.com>
2020-07-18 16:24:22 -0300

    [DIA-6] creates DISABLE_LOGIN env

--- public/env/config.js
+++ public/env/config.js
@@ -1,3 +1,4 @@
 window.USERS_URL = "http://localhost:8000";
 window.JWKS_URI = "http://localhost:4444/.well-known/jwks.json";
-window.JWKS_KID = "public:10d91b8b-df5f-491c-9a89-bc4f02e969b3";
+window.JWKS_KID = "public:9fdbe6c3-f641-4a42-b75e-bb4fcde2bfe2";
+window.DISABLE_LOGIN = true;
