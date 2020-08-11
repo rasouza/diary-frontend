e32f32f38e921b5b7fda403609efb4a06e43d5ad
R. A. Souza <alves.wm@gmail.com>
2020-08-11 00:13:55 -0300

    adds favicon

--- public/env/config.js
+++ public/env/config.js
@@ -1,4 +1,4 @@
 window.USERS_URL = "http://localhost:8000";
 window.JWKS_URI = "http://localhost:4444/.well-known/jwks.json";
-window.JWKS_KID = "public:9fdbe6c3-f641-4a42-b75e-bb4fcde2bfe2";
-window.DISABLE_LOGIN = true;
+window.JWKS_KID = "public:ef5f012e-10bc-4b66-a88b-5fcde520dc1f";
+window.DISABLE_LOGIN = false;
