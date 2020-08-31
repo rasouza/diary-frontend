a52fa4fd95478b90773c8828673acfbf5186bf9b
R. A. Souza <alves.wm@gmail.com>
2020-08-30 22:16:24 -0300

    adds docker files

--- basic/public/env/config.js
+++ basic/public/env/config.js
@@ -1,4 +1,4 @@
 window.USERS_URL = "http://localhost:8000";
 window.JWKS_URI = "http://localhost:4444/.well-known/jwks.json";
 window.JWKS_KID = "public:ef5f012e-10bc-4b66-a88b-5fcde520dc1f";
-window.DISABLE_LOGIN = false;
+window.DISABLE_LOGIN = true;
