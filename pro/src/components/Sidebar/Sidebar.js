ccb1d0f6fd7fc5220a6122a1259751c643846655
R. A. Souza <alves.wm@gmail.com>
2020-08-30 22:16:37 -0300

    changes logo and page title

--- pro/src/components/Sidebar/Sidebar.js
+++ pro/src/components/Sidebar/Sidebar.js
@@ -223,44 +223,17 @@ class Sidebar extends React.Component {
             <Nav navbar>{this.createLinks(routes)}</Nav>
             <hr className="my-3" />
             <h6 className="navbar-heading p-0 text-muted">
-              <span className="docs-normal">Documentation</span>
+              <span className="docs-normal">Read about</span>
               <span className="docs-mini">D</span>
             </h6>
             <Nav className="mb-md-3" navbar>
               <NavItem>
                 <NavLink
-                  href="https://demos.creative-tim.com/argon-dashboard-pro-react/#/documentation/overview?ref=adpr-sidebar"
+                  href="https://100daysofcode.com"
                   target="_blank"
                 >
-                  <i className="ni ni-spaceship" />
-                  <span className="nav-link-text">Getting started</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  href="https://demos.creative-tim.com/argon-dashboard-pro-react/#/documentation/colors?ref=adpr-sidebar"
-                  target="_blank"
-                >
-                  <i className="ni ni-palette" />
-                  <span className="nav-link-text">Foundation</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  href="https://demos.creative-tim.com/argon-dashboard-pro-react/#/documentation/alert?ref=adpr-sidebar"
-                  target="_blank"
-                >
-                  <i className="ni ni-ui-04" />
-                  <span className="nav-link-text">Components</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  href="https://demos.creative-tim.com/argon-dashboard-pro-react/#/documentation/charts?ref=adpr-sidebar"
-                  target="_blank"
-                >
-                  <i className="ni ni-chart-pie-35" />
-                  <span className="nav-link-text">Plugins</span>
+                  <i className="ni ni-book-bookmark" />
+                  <span className="nav-link-text">100 Days of Code</span>
                 </NavLink>
               </NavItem>
             </Nav>
