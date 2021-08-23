911719831edf8fc72d9022001f8cf167311cfe0e
R. A. Souza <alves.wm@gmail.com>
2021-08-23 01:06:27 -0300

    chore(argon): remove old template

--- argon/public/env/config.js
+++
@@ -1,5 +0,0 @@
-window.IDP_URL = "http://127.0.0.1:4433";
-window.JWKS_URI = "http://localhost:4444/.well-known/jwks.json";
-window.JWKS_KID = "public:b6adcd37-7162-4708-94bb-c01a947682ca";
-window.DISABLE_LOGIN = true;
-window.JWT_SECRET = "shhh"
