f1a8511105a5035d54b69ec590ee5f65652d9587
R. A. Souza <alves.wm@gmail.com>
2021-08-30 21:34:39 -0300

    feat(timeline): show user timeline for added stories

---
+++ src/pages/Story/components/TimelineCard.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  Badge,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown
+} from 'reactstrap'
+import moment from 'moment'
+import parse from 'html-react-parser'
+import classNames from 'classnames'
+
+moment.locale(window.navigator.language)
+
+export function TimelineCard({ story, inverted = false }) {
+  const { id, date, link, summary, thoughts } = story
+  return (
+    <li key={id} className={classNames({ 'timeline-inverted': inverted })}>
+      <div className="timeline-badge">
+        <i className="now-ui-icons education_agenda-bookmark" />
+      </div>
+      <div className="timeline-panel">
+        <div className="timeline-heading">
+          <Badge color="primary">{moment(date).format('LL')}</Badge>{' '}
+          {link && (
+            <a href={link} target="_blank" rel="noreferrer">
+              <Badge color="primary">
+                <i className="fas fa-globe-americas" /> Link
+              </Badge>
+            </a>
+          )}
+        </div>
+        <div className="timeline-body">
+          <p>{summary}</p>
+          <hr />
+          {thoughts && parse(thoughts)}
+        </div>
+        <div className="timeline-footer float-right">
+          <UncontrolledDropdown>
+            <DropdownToggle
+              className="btn-round"
+              color="primary"
+              data-toggle="dropdown"
+              type="button">
+              <i className="now-ui-icons ui-1_simple-remove" />
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
+                <span className="text-danger">Delete</span>
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+TimelineCard.propTypes = {
+  story: PropTypes.object.isRequired,
+  inverted: PropTypes.bool
+}
--- src/pages/Story/hooks/useStories.js
+++ src/pages/Story/hooks/useStories.js
@@ -1,4 +1,11 @@
 import { useQuery } from 'react-query'
 import { getStories } from '../api'
+import { splitEvery } from 'ramda'
+
+export const useStoriesByChunk = (chunk) =>
+  useQuery('stories', async () => {
+    const data = await getStories()
+    return splitEvery(chunk, data)
+  })
 
 export const useStories = () => useQuery('stories', getStories)
